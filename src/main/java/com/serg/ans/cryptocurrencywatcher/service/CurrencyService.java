e60599d3a9c1abcecd9124a220457b93ca55d206
SergeyAnshin <anshin.serega@gmail.com>
2022-07-13 21:02:07 +0300

    create CurrencyService and getObjectOnlyCurrencyType method

---
+++ src/main/java/com/serg/ans/cryptocurrencywatcher/service/CurrencyService.java
@@ -0,0 +1,22 @@
+package com.serg.ans.cryptocurrencywatcher.service;
+
+import com.serg.ans.cryptocurrencywatcher.entity.Currency;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CurrencyService {
+
+    public static List<Currency> getObjectOnlyCurrencyType(Iterator entities) {
+        List<Currency> currencies = new ArrayList<>();
+        Object entity;
+        while (entities.hasNext()) {
+            entity = entities.next();
+            if (entity instanceof Currency) {
+                currencies.add((Currency) entity);
+            }
+        }
+        return currencies;
+    }
+}
