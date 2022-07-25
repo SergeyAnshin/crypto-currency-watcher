c37f0e238429cce6c6e2be6d15bbe9dee749b9e9
SergeyAnshin <anshin.serega@gmail.com>
2022-07-25 23:52:26 +0300

    add constructor DI

--- src/main/java/com/serg/ans/cryptocurrencywatcher/repository/interceptor/PriceUpdateInterceptor.java
+++ src/main/java/com/serg/ans/cryptocurrencywatcher/repository/interceptor/PriceUpdateInterceptor.java
@@ -1,25 +1,17 @@
 package com.serg.ans.cryptocurrencywatcher.repository.interceptor;
 
-import com.serg.ans.cryptocurrencywatcher.entity.CryptoCurrency;
 import com.serg.ans.cryptocurrencywatcher.entity.Currency;
 import com.serg.ans.cryptocurrencywatcher.entity.MonitoredPosition;
 import com.serg.ans.cryptocurrencywatcher.service.CurrencyService;
-import com.serg.ans.cryptocurrencywatcher.service.MathOperationService;
 import com.serg.ans.cryptocurrencywatcher.service.MonitoredPositionService;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.CallbackException;
 import org.hibernate.EmptyInterceptor;
-import org.hibernate.type.Type;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Component
@@ -28,9 +20,8 @@ public class PriceUpdateInterceptor extends EmptyInterceptor {
     @Value("${percentChange}")
     private double percentChange;
 
-    @Autowired
     @Lazy
-    public void setMonitoredPositionService(MonitoredPositionService monitoredPositionService) {
+    public PriceUpdateInterceptor(MonitoredPositionService monitoredPositionService) {
         this.monitoredPositionService = monitoredPositionService;
     }
 
